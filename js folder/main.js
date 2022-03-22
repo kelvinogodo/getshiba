e3c8fcd49b1f0ed9ca04074433e3ee730fcf4823
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-03-22 18:54:48 +0000

    edited index.html

--- js folder/main.js
+++ js folder/main.js
@@ -5,14 +5,14 @@ let  sliders = document.querySelectorAll('.payment-card')
 window.addEventListener('scroll', ()=>{
     sliders.forEach(slider =>{
 
-        // let sliderHeight= slider.getBoundingClientRect('slider').top;
-        let sliderHeight= window.screenY;
-        let windowsHeight = window.innerHeight;
+        // // let sliderHeight= slider.getBoundingClientRect('slider').top;
+        // let sliderHeight= window.screenY;
+        // let windowsHeight = window.innerHeight;
 
-        if(windowsHeight  > sliderHeight){
-            // slider.classList.remove('animate');
-            console.log('im here')
-        }
+        // if(windowsHeight  > sliderHeight){
+        //     // slider.classList.remove('animate');
+        //     console.log('im here')
+        // }
 
     })
 });
