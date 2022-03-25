cb863a1770591250665995f8766528f8ab56f47d
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-03-25 12:19:38 +0000

    added some files

--- js folder/main.js
+++ js folder/main.js
@@ -16,3 +16,73 @@ window.addEventListener('scroll', ()=>{
 
     })
 });
+
+let faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(faqItem =>{
+    faqItem.addEventListener('click',
+     (e)=>{ 
+
+        let answer = (e.target.children[1]);
+
+            answer.style.height = '150px'; 
+        
+              faqItem.addEventListener('click',()=>{
+                  if(answer.style.height = '150px'){
+                    answer.style.height = '0px';
+                  }
+                  else{
+                        answer.style.height = '150px'; 
+                }
+        }) 
+    });
+ })
+
+
+
+ let links =document.querySelectorAll('.dashboard-links');
+
+ const route = (event)=>{
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({},'',event.target.href);
+    HandleLocation();
+ }
+ 
+
+ const routes = {
+     404:"../html folder/userhome.html",
+     "/home":"../html folder/userhome.html",
+     "/reinvest":"../html folder/reinvest.html",
+     "/referrals":"../html folder/referrals.html",
+     "/transactions":"../html folder/transactions.html",
+     "/plans":"../html folder/plans.html",
+     "/accountInfo":"../html folder/accountinfo.html",
+ };
+
+const HandleLocation = async ()=>{
+
+    const path = window.location.pathname;
+
+    const route = routes[path] || routes[404];
+    
+    const html = await fetch(route).then((data)=> data.text());
+
+    
+    document.querySelector(".page-displayer").innerHTML = html;
+
+}
+
+ links.forEach(link=>{
+     link.addEventListener('click', route);
+ })
+
+ window.onpopstate = HandleLocation;
+
+ window.route = route;
+ 
+ HandleLocation();
+
+ window.onload(()=>{
+    window.location.path="/html%20folder/dashboard.html";   
+ })
