aac5a706b877da300c4c9f296865aca5f10ba8a6
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-03-26 14:51:09 +0000

    edited some files

--- js folder/main.js
+++ js folder/main.js
@@ -28,7 +28,7 @@ faqItems.forEach(faqItem =>{
             answer.style.height = '150px'; 
         
               faqItem.addEventListener('click',()=>{
-                  if(answer.style.height = '150px'){
+                  if(answer.style.height ='150px'){
                     answer.style.height = '0px';
                   }
                   else{
@@ -40,49 +40,65 @@ faqItems.forEach(faqItem =>{
 
 
 
- let links =document.querySelectorAll('.dashboard-links');
+//  let links =document.querySelectorAll('.dashboard-links');
 
- const route = (event)=>{
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({},'',event.target.href);
-    HandleLocation();
- }
+//  const route = (event)=>{
+//     event = event || window.event;
+//     event.preventDefault();
+//     window.history.pushState({},'',event.target.href);
+//     HandleLocation();
+//  }
  
 
- const routes = {
-     404:"../html folder/userhome.html",
-     "/home":"../html folder/userhome.html",
-     "/reinvest":"../html folder/reinvest.html",
-     "/referrals":"../html folder/referrals.html",
-     "/transactions":"../html folder/transactions.html",
-     "/plans":"../html folder/plans.html",
-     "/accountInfo":"../html folder/accountinfo.html",
- };
+//  const routes = {
+//      404:"../html folder/userhome.html",
+//      "/home":"../html folder/userhome.html",
+//      "/reinvest":"../html folder/reinvest.html",
+//      "/referrals":"../html folder/referrals.html",
+//      "/transactions":"../html folder/transactions.html",
+//      "/plans":"../html folder/plans.html",
+//      "/accountInfo":"../html folder/accountinfo.html",
+//  };
 
-const HandleLocation = async ()=>{
+// const HandleLocation = async ()=>{
 
-    const path = window.location.pathname;
+//     const path = window.location.pathname;
 
-    const route = routes[path] || routes[404];
+//     const route = routes[path] || routes[404];
     
-    const html = await fetch(route).then((data)=> data.text());
+//     const html = await fetch(route).then((data)=> data.text());
 
     
-    document.querySelector(".page-displayer").innerHTML = html;
+//     document.querySelector(".page-displayer").innerHTML = html;
 
+// }
+
+//  links.forEach(link=>{
+//      link.addEventListener('click', route);
+//  })
+
+//  window.onpopstate = HandleLocation;
+
+//  window.route = route;
+ 
+//  HandleLocation();
+
+//  window.onload(()=>{
+//     window.location.path="/html%20folder/dashboard.html";   
+//  })
+
+// menu bar codes
+
+
+function OpenMenubar(){
+    const menuList=document.getElementById('menu-items');
+    menuList.style.height='80vh';   
 }
-
- links.forEach(link=>{
-     link.addEventListener('click', route);
- })
-
- window.onpopstate = HandleLocation;
-
- window.route = route;
- 
- HandleLocation();
-
- window.onload(()=>{
-    window.location.path="/html%20folder/dashboard.html";   
- })
+function CloseSideBar(){
+    const menuList=document.getElementById('menu-items');
+    menuList.style.height='0px';
+}
+const menu=document.getElementById('menubar');  
+menu.addEventListener('click',OpenMenubar);
+const sidebar=document.getElementById('close-sidebar');  
+sidebar.addEventListener('click',CloseSideBar);
